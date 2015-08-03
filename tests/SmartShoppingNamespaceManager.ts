294f24ea27dfc6b2b6e11ce4b5dc69310e414443
Simon Urli <simon.urli@gmail.com>
2015-08-03 20:50:00 +0200

    SERVICES-75 Setup tests for service smartshopping

---
+++ tests/SmartShoppingNamespaceManager.ts
@@ -0,0 +1,15 @@
+/**
+ * @author Simon Urli <simon@the6thscreen.fr>
+ */
+
+/// <reference path="../t6s-core/core-backend/t6s-core/core/libsdef/mocha.d.ts" />
+
+/// <reference path="../scripts/SmartShoppingNamespaceManager.ts" />
+
+var assert = require("assert");
+
+describe('SmartShoppingNamespaceManager', function() {
+	it('should always pass', function () {
+		assert.ok(true);
+	});
+});
